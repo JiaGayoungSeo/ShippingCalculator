3450c13b92d85f1ad72549d38b06576efc93a43d
Jia Gayoung Seo <gayoungs9397@gmail.com>
2020-01-11 08:10:39 -0600

    created all calculating function

--- app/src/main/java/com/example/shippingcalculator/MainActivity.java
+++ app/src/main/java/com/example/shippingcalculator/MainActivity.java
@@ -1,18 +1,80 @@
 package com.example.shippingcalculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ShipItem shipItem;
-
+    private EditText weightET;
+    private TextView baseCostTV;
+    private TextView addedCostTV;
+    private TextView totalCostTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        shipItem = new ShipItem();
+        weightET = findViewById(R.id.editText1);
+
+        baseCostTV = findViewById(R.id.textView4);
+        addedCostTV = findViewById(R.id.textView6);
+        totalCostTV = findViewById(R.id.textView8);
+
+        weightET.addTextChangedListener(weightTextWatcher);
+    }
+
+    private TextWatcher weightTextWatcher = new TextWatcher(){
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try{
+                shipItem.setWeight((int)Double.parseDouble(s.toString()));
+            }catch (NumberFormatException e){
+                shipItem.setWeight(0);
+            }
+            displayShipping();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+    };
+
+    private void displayShipping(){
+        baseCostTV.setText("$"+ String.format("%.02f", shipItem.getBaseCost()));
+        addedCostTV.setText("$"+String.format("%.02f",shipItem.getAddedCost()));
+        totalCostTV.setText("$"+String.format("%.02f",shipItem.getTotalCost()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.my,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
